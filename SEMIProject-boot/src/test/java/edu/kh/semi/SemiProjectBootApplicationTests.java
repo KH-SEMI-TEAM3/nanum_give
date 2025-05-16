0fe549349e16ab2f6336fe2da8cc039d86d25e20
kichanwon <wgichan@gmail.com>
2025-05-16 12:02:41 +0900

    project setting (ing)

---
+++ SEMIProject-boot/src/test/java/edu/kh/semi/SemiProjectBootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kh.semi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiProjectBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
