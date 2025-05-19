c29fb9590a5924e21f8aa46fb2eb9cce2a96e0d7
이주원 <neosiureu@gmail.com>
2025-05-19 15:36:37 +0900

    메시지 컨트롤러부터 매퍼까지

--- SEMIProject-boot/src/main/java/edu/kh/semi/member/model/service/MemberService.java
+++ SEMIProject-boot/src/main/java/edu/kh/semi/member/model/service/MemberService.java
@@ -3,7 +3,42 @@ package edu.kh.semi.member.model.service;
 import edu.kh.semi.member.model.dto.Member;
 
 public interface MemberService {
+	
+	/** 로그인 서비스
+	 * @param inputMember
+	 * @return loginMember(Member)
+	 */
+	Member login(Member inputMember);
+	
+	/** 회원 아이디 중복 검사 서비스
+	 * @param memberId
+	 * @return
+	 */
+	int checkMemberId(String memberId);
 
+	/** 이메일 중복 검사 서비스
+	 * @param memberEmail
+	 * @return
+	 */
+	int checkEmail(String memberEmail);
+
+	/** 닉네임 중복 검사 서비스
+	 * @param memberNickname
+	 * @return
+	 */
+	int checkNickname(String memberNickname);
+
+	/** 회원가입 서비스
+	 * @param inputMember
+	 * @param memberAddress
+	 * @return
+	 */
+	int signup(Member inputMember, String[] memberAddress);
+
+	/** 멤버의 전체 정보를 memberNo을 주면 반환해주는 로직
+	 * @param memberNo
+	 * @return
+	 */
 	Member selectMemberByNo(int memberNo);
-  
+
 }
--- SEMIProject-boot/src/main/java/edu/kh/semi/message/model/service/MessageService.java
+++ SEMIProject-boot/src/main/java/edu/kh/semi/message/model/service/MessageService.java
@@ -9,15 +9,41 @@ import edu.kh.semi.message.model.dto.Message;
 
 public interface MessageService {
 
+	
+	
+	/** 메시지 보내기
+	 * @param message
+	 * @return
+	 */
 	int sendMessage(Message message);
 
-	Message getMessageDetail(Map<String, Object> paramMap);
-
+	
+	/** 메시지 삭제
+	 * @param messages
+	 * @return
+	 */
 	int deleteMessagePage(Message messages);
 
-	List<Message> getInboxMessages(int memberNo);
+	
+	/** 메시지 상세내용 조회 (보내는거든 받는거든 똑같이 함)
+	 * @param paramMap
+	 * @return
+	 */
+	Message getMessageDetail(Map<String, Object> paramMap);
 
-	List<Message> getSentMessages(int memberNo);
+	/** 보낸 메시지 모음
+	 * @param memberNo
+	 * @return
+	 */
+	List<Message> selectSentMessages(int memberNo);
+	
+	/** 받은 메시지 모음
+	 * @param memberNo
+	 * @return
+	 */
+	List<Message> selectReceiveMessages(int memberNo);
+
+
 
 
 }
